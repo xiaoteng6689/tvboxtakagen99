c0b9814cbf1e004cc06666cdb6d15f8cce4da8ee
kriou <39736030+kriou@users.noreply.github.com>
2022-12-25 12:41:45 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -65,7 +65,7 @@ public class App extends MultiDexApplication {
         putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
         putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
 //        putDefault(HawkConfig.HOME_NUM, 2);       // History Number
-//        putDefault(HawkConfig.DOH_URL, 2);        // DNS
+        putDefault(HawkConfig.DOH_URL, 2);        // DNS
 //        putDefault(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture
 
     }

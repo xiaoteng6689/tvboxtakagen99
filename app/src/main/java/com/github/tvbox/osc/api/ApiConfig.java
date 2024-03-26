71c1a2c2d0b548b3e80dca1be9f0ae68f285ffa0
jxgzs <52615372+jxgzs@users.noreply.github.com>
2024-03-26 12:52:00 -0400

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -131,7 +131,7 @@ public class ApiConfig {
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
         // Embedded Source : Update in Strings.xml if required
-        String apiUrl = Hawk.get(HawkConfig.API_URL, HomeActivity.getRes().getString(R.string.app_source));
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://nycvod.com/tv/1.json");
         if (apiUrl.isEmpty()) {
             callback.error("源地址为空");
             return;

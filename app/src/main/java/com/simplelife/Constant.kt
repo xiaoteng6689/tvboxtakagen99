97f81bf284d0386f33198cacee5def66b6ecc4db
xuanzhu <2389752245@qq.com>
2024-05-06 21:41:45 +0800

    更改更新地址以iptv地址

--- app/src/main/java/com/simplelife/Constant.kt
+++ app/src/main/java/com/simplelife/Constant.kt
@@ -10,7 +10,7 @@ package com.simplelife
  */
 object Constant {
     const val FILE_NAME = "IPTV.m3u"
-    const val URL = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Potato-66/SimpleTV/main/m3u/ipv6/IPTV.m3u"
+    const val URL = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/wangxuanzhu/Version/main/TVBox/ipv6/IPTV.m3u"
     const val KEY_LAST_CHANNEL = "last_channel"
     const val KEY_BOOT_STARTUP = "boot_startup"
     const val KEY_AUTO_UPDATE = "auto_update"
@@ -18,7 +18,7 @@ object Constant {
     const val DIALOG_TAG_SETTING = "setting"
     const val DIALOG_TAG_CHANNEL = "channel"
     const val DIALOG_TAG_UPDATE = "update"
-    const val URL_VERSION = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Potato-66/Version/main/SimpleTV/versionInfo.json"
-    const val URL_APK = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Potato-66/Version/main/SimpleTV/SimpleTV.apk"
+    const val URL_VERSION = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/wangxuanzhu/Version/main/TVBox/versionInfo.json"
+    const val URL_APK = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/wangxuanzhu/Version/main/TVBox/TVBox.apk"
     const val APK_NAME = "TVBox.apk"
 }

1e7aa4017cbafb07bad141c1656dea5620ee6ea0
sayxiaoyan <127412316+sayxiaoyan@users.noreply.github.com>
2024-06-04 22:11:05 +0800

    还原回原来的EXO解码，去除第三方anilbeesetti库的nextlib-media3ext解码 (#142)
    * 回滚代码：新增加的media3ext:0.5.0会导致高分辨率的视频播放一帧一帧的
    
    回滚（增加exo解码能力，解决部分视频exo,ijk解码都是有声音没图像的问题，引入的第三方库要求最低5.0）
    
    * 回滚代码：新增加的media3ext:0.5.0会导致高分辨率的视频播放一帧一帧的
    
    回滚（增加exo解码能力，解决部分视频exo,ijk解码都是有声音没图像的问题，引入的第三方库要求最低5.0）
    
    * 回滚代码：新增加的media3ext:0.5.0会导致高分辨率的视频播放一帧一帧的
    
    回滚（增加exo解码能力，解决部分视频exo,ijk解码都是有声音没图像的问题，引入的第三方库要求最低5.0）

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -19,11 +19,9 @@ import androidx.media3.exoplayer.DefaultLoadControl;
 import androidx.media3.exoplayer.DefaultRenderersFactory;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.exoplayer.LoadControl;
-import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
 import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
 import androidx.media3.exoplayer.trackselection.TrackSelectionArray;
-import androidx.media3.extractor.ExtractorsFactory;
 import androidx.media3.ui.PlayerView;
 
 import com.github.tvbox.osc.base.App;
@@ -35,7 +33,6 @@ import com.orhanobut.hawk.Hawk;
 import java.util.Locale;
 import java.util.Map;
 
-import io.github.anilbeesetti.nextlib.media3ext.ffdecoder.NextRenderersFactory;
 import xyz.doikki.videoplayer.player.AbstractPlayer;
 import xyz.doikki.videoplayer.util.PlayerUtils;
 
@@ -71,7 +68,7 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
     @Override
     public void initPlayer() {
         if (mRenderersFactory == null) {
-            mRenderersFactory = new NextRenderersFactory(mAppContext);
+            mRenderersFactory = new DefaultRenderersFactory(mAppContext);
         }
         //https://github.com/androidx/media/blob/release/libraries/decoder_ffmpeg/README.md
         if ("MiTV-MFTR0".equals(Build.MODEL)) {
@@ -98,7 +95,6 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         mMediaPlayer = new ExoPlayer.Builder(mAppContext)
                 .setLoadControl(mLoadControl)
                 .setRenderersFactory(mRenderersFactory)
-                .setMediaSourceFactory(new DefaultMediaSourceFactory(mAppContext, ExtractorsFactory.EMPTY))
                 .setTrackSelector(mTrackSelector).build();
 
         setOptions();

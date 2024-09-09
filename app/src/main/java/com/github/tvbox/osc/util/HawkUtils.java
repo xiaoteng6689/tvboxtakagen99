06bfe75bffefbef060785408d5712a960779ec79
Swifly <cyf783@qq.com>
2024-09-09 08:44:33 +0800

    支持弹幕 (#161)

--- app/src/main/java/com/github/tvbox/osc/util/HawkUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkUtils.java
@@ -15,6 +15,59 @@ import java.util.List;
 import io.github.anilbeesetti.nextlib.media3ext.ffdecoder.NextRenderersFactory;
 
 public class HawkUtils {
+
+    private static final String DANMU_OPEN = "danmu_open";
+    private static final String DANMU_MAXLINE = "danmu_maxline";
+    private static final String DANMU_SPEED = "danmu_speed";
+    private static final String DANMU_ALPHA = "danmu_alpha";
+    private static final String DANMU_SIZESCALE = "danmu_sizescale";
+    private static final String DANMU_COLOR = "danmu_color";
+
+    public static boolean getDanmuOpen() {
+        return Hawk.get(DANMU_OPEN, true);
+    }
+
+    public static void setDanmuOpen(boolean danmuOpen) {
+        Hawk.put(DANMU_OPEN, danmuOpen);
+    }
+
+    public static int getDanmuMaxLine() {
+        return Hawk.get(DANMU_MAXLINE, 3);
+    }
+
+    public static void setDanmuMaxLine(int danmuMaxLine) {
+        Hawk.put(DANMU_MAXLINE,danmuMaxLine);
+    }
+
+    public static float getDanmuSpeed() {
+        return Hawk.get(DANMU_SPEED, 1.5f);
+    }
+
+    public static void setDanmuSpeed(float danmuSpeed) {
+        Hawk.put(DANMU_SPEED,danmuSpeed);
+    }
+
+    public static float getDanmuAlpha() {
+        return Hawk.get(DANMU_ALPHA, 90 / 100.0f);
+    }
+
+    public static void setDanmuAlpha(float danmuAlpha) {
+        Hawk.put(DANMU_ALPHA,danmuAlpha);
+    }
+
+    public static float getDanmuSizeScale() {
+        return Hawk.get(DANMU_SIZESCALE, 0.8f);
+    }
+
+    public static void setDanmuSizeScale(float danmuSizeScale) {
+        Hawk.put(DANMU_SIZESCALE,danmuSizeScale);
+    }
+    public static boolean getDanmuColor() {
+        return Hawk.get(DANMU_COLOR, false);
+    }
+    public static void setDanmuColor(boolean color) {
+        Hawk.put(DANMU_COLOR,color);
+    }
     public static String getIJKCodec() {
         return Hawk.get(HawkConfig.IJK_CODEC, "");
     }
